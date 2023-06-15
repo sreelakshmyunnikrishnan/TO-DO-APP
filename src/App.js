052ea03d3a48b5aef24c2a2e633f8cc421331137
Sreelakshmy Unnikrishnan <sreelakshmy2001@gmail.com>
2023-06-15 11:50:45 +0530

    Add tasks to Todo app
    Signed-off-by: Sreelakshmy Unnikrishnan <sreelakshmy2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,21 +2,49 @@ import React, { useState } from "react";
 import "./App.css";
 
 // React functional component to create todo application
-const TodoApp = () => {
-  const [addTask, setAddTask] = useState("No Task Added");
 
-// Arrow function triggered when the user clicks 
-  const addTaskButtonClick = () =>{
-    setAddTask("Task Created")
-  }
+const TodoApp = () => {
+  const [value, setValue] = useState("");
+  const [taskList, setAddTask] = useState([]);
+  const [edit, setEditValue] = useState("");
+
+  // When the user clicks "Add Task" button, the arrow function is triggered
+  const addTaskButtonClick = () => {
+    // Create a copy of task list
+    const tempTaskList = Array.from(taskList);
+    // Add new task to the copy
+    tempTaskList.push(value);
+    // Update the task list
+    setAddTask(tempTaskList);
+
+    // Or
+
+    // setAddTask([...taskList, value])
+
+  };
 
   return (
     <div className="App">
       <h2>ToDo App </h2>
-      <h5>{addTask}</h5>
-      <button onClick={addTaskButtonClick} >
-        Add a Task
-      </button>
+
+      {/* Input attribute takes in user input */}
+      <input
+        type="text"
+        // Onchange is triggered when the value of user input is changed or if a new task is added
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+
+      <button onClick={addTaskButtonClick}>Add a Task</button>
+
+      {/* Iterate through the taskList array to display it as a list */}
+
+      <ul style={{ listStyleType: "none" }}>
+        {taskList.map((element, index) => (
+          <li key={index}>
+            <span>{element}{" "}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
